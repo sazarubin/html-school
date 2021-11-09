aa956423fe57c434782f4abda661e585a6a55a9f
Sergey <s.zarubin@cakeisnull.com>
2021-11-09 11:50:02 +0300

    fixes

---
+++ script/menu.js
@@ -0,0 +1,4 @@
+document.querySelector(".burger-button").addEventListener("click", (e) => {
+  document.querySelector(".nav-menu").classList.toggle("hide");
+  document.querySelector(".burger-button").classList.toggle("cross");
+});
